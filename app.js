f2bf231830b041d95346055cbe447b785b064a5c
Leandro <leandrovoupassar@gmail.com>
2025-08-17 19:27:06 -0300

    mudança de teste

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ titulo.innerHTML = "Jogo do Número Secreto"; // Altera o conteúdo do elemento 
 // querySelectorAll() selecioan todos os elementos
 
 /*let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = "Escolha um número entre 1 e 100";*/
+paragrafo.innerHTML = "Escolha um número entre 1 e 10";*/
 
 //fazer acima ou resumir a chamada das tags abaixo
 
